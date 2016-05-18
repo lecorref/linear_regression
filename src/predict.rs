1144ba12bca360aab7206cdae999ed86f23dcd8b
francois LE CORRE <lecorre.f@gmail.com>
2016-05-18 13:09:34 +0200

    it works

--- src/predict.rs
+++ src/predict.rs
@@ -2,14 +2,16 @@ use std::io;
 use std::io::Read;
 use std::fs::File;
 
-fn read_file(mut file: std::fs::File, mileage: i32) -> (){
+fn read_file(mut file: std::fs::File, mileage: f64) -> (){
     let mut input = String::new();
 
     file.read_to_string(&mut input).expect("Cannot read file");
     let vec = input.split(" ").collect::<Vec<&str>>();
-    let theta0: i32 = vec[0].trim().parse().expect("Theta0 is not a number");
-    let theta1: i32 = vec[1].trim().parse().expect("Theta1 is not a number");
-    println!("Price: {}", theta0 + (theta1 * mileage));
+    let theta0: f64 = vec[0].trim().parse().expect("Theta0 is not a number");
+    let theta1: f64 = vec[1].trim().parse().expect("Theta1 is not a number");
+    let min: f64 = vec[2].trim().parse().expect("Theta0 is not a number");
+    let max: f64 = vec[3].trim().parse().expect("Theta1 is not a number");
+    println!("Price: {}", theta0 + (theta1 * (mileage - min) / (max - min)));
 }
 
 fn main() {
@@ -17,7 +19,7 @@ fn main() {
 
     println!("Please enter a mileage:");
     io::stdin().read_line(&mut input).expect("Error on read");
-    let mileage: i32 = input.trim().parse().expect("Not a Number");
+    let mileage: f64 = input.trim().parse().expect("Not a Number");
 
     match File::open("/tmp/frale-co/data.txt") {
        Ok(res) => {read_file(res, mileage)},
--- src/train.rs
+++ src/train.rs
@@ -1,6 +1,30 @@
 extern crate clap;
+extern crate csv;
 use clap::{Arg, App};
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+type Row = (f64, f64);
 
+fn create_file(t0: f64, t1: f64, min: f64, max: f64) -> () {
+    let path = Path::new("/tmp/frale-co/data.txt");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    match file.write_all(format!("{} {} {} {}", t0, t1, min, max).as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {}: {}", display,
+                   Error::description(&why))
+        },
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
 fn main() {
     let matches = App::new("Training program")
         .version("1.0")
@@ -9,6 +33,30 @@ fn main() {
              .required(true)
              .index(1))
         .get_matches();
-    println!("{:?}", String::from(matches.value_of("DATASET").unwrap()));
+    let mut rdr = csv::Reader::from_file(String::from(matches.value_of("DATASET").unwrap())).unwrap();
+    let rows = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
 
+    let &(maxx, _) = rows.iter().max_by_key(|&&(x, _)| x as i32).unwrap();
+    let &(minx, _) = rows.iter().min_by_key(|&&(x, _)| x as i32).unwrap();
+
+ //   let &(_, maxy) = rows.iter().max_by_key(|&&(_, x)| x as i32).unwrap();
+ //   let &(_, miny) = rows.iter().min_by_key(|&&(_, x)| x as i32).unwrap();
+
+    let scaledrows = rows.iter().map(|&(x, y)| ((x - minx) / (maxx - minx), y)).collect::<Vec<Row>>();
+    let len: f64 = rows.len() as f64;
+    let learning_rate: f64 = 0.1;
+
+    let mut theta0: f64 = 0.0;
+    let mut theta1: f64 = 0.0;
+
+    for _ in 1..1000 {
+        let (test, test1) = scaledrows.iter().fold((0.0, 0.0), |(x, y), &(km, price)|{
+            let theta = ((theta0 + theta1 * km) - price) / len;
+            (x + theta, y + theta * km)
+        }
+        );
+        theta0 -= test * learning_rate;
+        theta1 -= test1 * learning_rate;
+    }
+    create_file(theta0, theta1, minx, maxx);
 }
